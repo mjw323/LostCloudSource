8a6d89a0aefe3ec4bd06a9d5149e9fc835efb54e
unknown <zchgre@gmail.com>
2013-10-20 15:34:38 -0400

    Hoverboard movement and camera following. Hoverboard cannot currently reorient itself without interfering with turning/steering.

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,17 @@
+﻿// PlayerMovementController.cs
+// Author: Zach Greer
+using UnityEngine;
+using System.Collections;
+
+// Signals intent to an attached Movement component.
+public class PlayerMovementController : MonoBehaviour {
+	HoverMovement hoverMovement;
+
+	void Start() {
+		hoverMovement = GetComponent<HoverMovement>();
+	}
+	
+	void Update() {
+		hoverMovement.Move(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+	}
+}
