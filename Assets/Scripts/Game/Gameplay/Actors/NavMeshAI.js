449c297af1b2ab31ea119f9ff2ec7af513f2d68a
Wolfe <mjw323@n1-141-195.dhcp.drexel.edu>
2013-11-09 14:40:05 -0500

    added debug level to test AI changes

--- Assets/Scripts/Game/Gameplay/Actors/NavMeshAI.js
+++ Assets/Scripts/Game/Gameplay/Actors/NavMeshAI.js
@@ -1,6 +1,36 @@
 ﻿var target : Transform;
+var Leaping : boolean;
+var leapDistance : float;
+
+
+leapDistance = 25.0f;
 
 function Update(){
-	GetComponent(NavMeshAgent).destination = target.position;
+	
+	if (!Leaping && (Vector3.Magnitude(target.position - this.transform.position) < 50)){
+		GetComponent(NavMeshAgent).destination = target.position;
+	}
+	
+	if (Random.Range(1,500) == 1 || Leaping){
+	if (!Leaping){Debug.Log("Leap!");}
+		leap();
+		
+	}
+	
 
+}
+
+function leap(){
+	var distance : float;
+	distance = Vector3.Magnitude(target.position-this.transform.position);
+	GetComponent(NavMeshAgent).destination = target.position + (target.rigidbody.velocity * (distance/GetComponent(NavMeshAgent).speed));
+	if (distance > leapDistance){
+		Leaping = true;
+		GetComponent(NavMeshAgent).speed = 200;
+	}
+	else{
+		Debug.Log("OK done!!");
+		Leaping = false;
+		GetComponent(NavMeshAgent).speed = 12;
+	}
 }
