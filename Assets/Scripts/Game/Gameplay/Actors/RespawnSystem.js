3777aa75777eefb81b4a0d2312e03f40bf0aa7ee
Greg Lobanov <greg@dumbandfat.com>
2013-12-28 23:39:28 -0500

    Island geometry re-placed, respawn start

---
+++ Assets/Scripts/Game/Gameplay/Actors/RespawnSystem.js
@@ -0,0 +1,30 @@
+﻿var SpawnPoint : Vector3;
+var Player : Transform;
+var MainCamera : Camera;
+var FadeWaitTime : int;
+var FootDist : float;  //distance below player to check for ground
+
+
+
+function Start(){
+	SpawnPoint = Player.position;
+}
+
+function Update(){
+	var hit : RaycastHit;
+		if (Physics.Raycast (Player.position, -Vector3.up*FootDist, hit)) {
+			if ((hit.transform.tag!="Grind") && (hit.transform.tag!="Water") && (hit.transform.tag!="NoRespawn")){
+				SpawnPoint = Player.position;
+			}
+		}
+}
+
+
+
+function Respawn()
+	{
+		MainCamera.SendMessage("fadeOut");
+		yield WaitForSeconds(FadeWaitTime);
+		MainCamera.SendMessage("fadeIn");
+		Player.position = SpawnPoint;
+	}
