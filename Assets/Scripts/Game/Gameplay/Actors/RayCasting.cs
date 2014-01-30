e6b329acda38a5a2fbe7ed537e95bcbecf9228a0
Michael Wolfe <mjw323@drexel.edu>
2014-01-30 15:00:08 -0500

    added a square to help test casting for the meeting with Stefan

--- Assets/Scripts/Game/Gameplay/Actors/RayCasting.cs
+++ Assets/Scripts/Game/Gameplay/Actors/RayCasting.cs
@@ -6,8 +6,8 @@ public class RayCasting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 forward = transform.TransformDirection(Vector3.forward) ;
-		if(Physics.Raycast(transform.position, transform.forward,  50)){
-			Debug.DrawRay(transform.position, transform.forward*5, Color.green);
+		if(Physics.Raycast(transform.position, transform.forward,  200)){
+			Debug.DrawRay(transform.position, transform.forward*10, Color.green);
 		}
 
 		RaycastHit  hit;                                                                                                                                                
