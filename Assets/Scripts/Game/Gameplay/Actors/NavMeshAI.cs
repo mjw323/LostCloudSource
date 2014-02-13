7086d993bfeb9dd43985accc6f53c35e982deaa0
Michael Wolfe <mjw323@drexel.edu>
2014-02-13 15:00:05 -0500

    Made Yorex not glide anymore, moved raycasts to Yorex to control looking at the player

--- Assets/Scripts/Game/Gameplay/Actors/NavMeshAI.cs
+++ Assets/Scripts/Game/Gameplay/Actors/NavMeshAI.cs
@@ -15,7 +15,36 @@ public class NavMeshAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		Vector3 forward = transform.TransformDirection(Vector3.forward) ;
+		Vector3 downoffset = new Vector3(0,-2,0);
+		Vector3 rightoffset = new Vector3(3,0,0);
+		Vector3 upoffset = new Vector3(0,2,0);
+		Vector3 leftoffset = new Vector3(-3,0,0);
+		
+		RaycastHit hit;
 		if (Player.GetComponent<UpgradeSystem>().HasPlayerGottenNextUpgrade){
+			if(Physics.SphereCast(gameObject.transform.position, 3, transform.forward, out hit, 20000) && hit.transform.gameObject.tag == "Player")
+				transform.LookAt(hit.transform);
+				Debug.DrawRay(transform.position,transform.forward*10,Color.green);
+		
+			if(Physics.SphereCast(gameObject.transform.position + upoffset, 3, transform.forward, out hit, 20000) && hit.transform.gameObject.tag == "Player")
+				transform.LookAt(hit.transform);
+				Debug.DrawRay(transform.position + upoffset,transform.forward*10,Color.green);
+		
+			if(Physics.SphereCast(gameObject.transform.position + downoffset, 3, transform.forward, out hit, 20000) && hit.transform.gameObject.tag == "Player")
+				transform.LookAt(hit.transform);
+				Debug.DrawRay(transform.position + downoffset,transform.forward*10,Color.green);
+		
+			if(Physics.SphereCast(gameObject.transform.position + leftoffset, 3, transform.forward + leftoffset, out hit, 20000) && hit.transform.gameObject.tag == "Player")
+				transform.LookAt(hit.transform);
+				Debug.DrawRay(gameObject.transform.position + leftoffset,transform.forward*10,Color.green);
+		
+			if(Physics.SphereCast(gameObject.transform.position + rightoffset, 3, transform.forward + rightoffset, out hit, 20000) && hit.transform.gameObject.tag == "Player")
+				transform.LookAt(hit.transform);
+				Debug.DrawRay(gameObject.transform.position + rightoffset,transform.forward*10,Color.green);
+
+
 			if (!Leaping && (Vector3.Magnitude(Player.position - this.transform.position) < 100)){
 				GetComponent<NavMeshAgent>().destination = Player.position;
 			}
