e871cb3b7f855ab5485bc30e5d57e861abcb395d
openglsdl <openglsdl@gmail.com>
2014-02-13 11:26:57 -0500

    Minor changes, testing grass.

---
+++ Assets/Scripts/Game/Environment/Grass.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Grass : MonoBehaviour {
+    private MeshFilter mf = null;
+    private MeshRenderer mr = null;
+    private GameObject[] grass;
+    private const int GRASS_COUNT = 32;
+
+	// Use this for initialization
+	void Start() {
+        //InitializeGrass();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void InitializeGrass() {
+        Vector3 offset = Vector3.zero;
+        grass = new GameObject[GRASS_COUNT * 3];
+
+        for (int idx = 0; idx < GRASS_COUNT * 3; idx+=3)
+        {
+            grass[idx] = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            grass[idx].name = "GrassChild_" + (idx / 3) + "_a";
+            grass[idx].transform.parent = transform;
+            grass[idx].transform.position = transform.position + offset;
+            grass[idx].renderer.castShadows = false;
+            grass[idx].renderer.receiveShadows = false;
+            grass[idx].renderer.material = renderer.material;
+
+            grass[idx + 1] = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            grass[idx + 1].name = "GrassChild_" + (idx / 3) + "_b";
+            grass[idx + 1].transform.parent = transform;
+            grass[idx + 1].transform.position = transform.position + offset;
+            grass[idx + 1].transform.Rotate(Vector3.up, 45.0f);
+            grass[idx + 1].renderer.castShadows = false;
+            grass[idx + 1].renderer.receiveShadows = false;
+            grass[idx + 1].renderer.material = renderer.material;
+
+            grass[idx + 2] = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            grass[idx + 2].name = "GrassChild_" + (idx / 3) + "_c";
+            grass[idx + 2].transform.parent = transform;
+            grass[idx + 2].transform.position = transform.position + offset;
+            grass[idx + 2].transform.Rotate(Vector3.up, -45.0f);
+            grass[idx + 2].renderer.castShadows = false;
+            grass[idx + 2].renderer.receiveShadows = false;
+            grass[idx + 2].renderer.material = renderer.material;
+
+            offset.x += (2.0f * Mathf.Cos(Mathf.Repeat(idx, 360.0f)));
+            offset.z += (2.0f * Mathf.Sin(Mathf.Repeat(idx, 360.0f)));
+        }
+    }
+}
---
+++ Assets/Shaders/Grass.shader
@@ -0,0 +1,32 @@
+﻿Shader "LostCloud/Grass" {
+	Properties {
+		_GrassTex ("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="TransparentCutout" "Queue"="AlphaTest" }
+		LOD 300
+        
+        // Draw double-sided
+        //Cull Off
+        //ZWrite Off
+		//AlphaTest Greater 0.2
+
+		CGPROGRAM
+		#pragma surface surf Lambert
+		#pragma target 3.0
+
+		sampler2D _GrassTex;
+
+		struct Input {
+			float2 uv_GrassTex;
+		};
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			half4 c = tex2D (_GrassTex, IN.uv_GrassTex);
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	} 
+	FallBack "Diffuse"
+}
