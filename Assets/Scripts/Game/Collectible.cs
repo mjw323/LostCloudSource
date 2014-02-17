5f2c100a6e2c5f11947d95f852fbaeebadda76f6
gl326 <gl326@n1-142-250.dhcp.drexel.edu>
2014-02-16 21:24:32 -0500

    start on collectible system

---
+++ Assets/Scripts/Game/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible : MonoBehaviour {
+	public bool collected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Game/Player/CollectibleSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleSystem : MonoBehaviour {
+	public int upgradeCount;
+	public int upgradesFound = 0;
+
+	// Use this for initialization
+	void Start () {
+		upgradeCount = GameObject.FindGameObjectsWithTag ("Collectible").Length ();
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.tag == "Collectible") {
+			Collectible c = col.gameObject.GetComponent<Collectible>();
+			if (!c.collected){
+				c.collected = true;
+				upgradesFound += 1;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
