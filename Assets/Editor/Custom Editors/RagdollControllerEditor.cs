335f77643fe2a610d0899d453aa6db856387f2b9
Zach Greer <zchgre@gmail.com>
2014-02-24 14:41:05 -0500

    Custom editor for RagdollController. You can now force a ragdoll using a button in the inspector.

---
+++ Assets/Editor/Custom Editors/RagdollControllerEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RagdollController))]
+public class RagdollControllerEditor : Editor {
+	public void OnEnable() {
+		instance = (RagdollController)target;
+	}
+
+	public override void OnInspectorGUI() {
+		if (instance == null) {
+			return;
+		}
+
+		DrawDefaultInspector();
+		// Disable the use of this button outside of Play Mode.
+		if (!EditorApplication.isPlaying) {
+			GUI.enabled = false;
+		}
+		if (GUILayout.Button("Ragdoll")) {
+			instance.DoRagdoll();
+		}
+		GUI.enabled = true;
+	}
+
+	private RagdollController instance;
+}
---
+++ Assets/Scripts/Game/Player/RagdollController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RagdollController : MonoBehaviour {
+	public void DoRagdoll() {
+		Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+		for (int i = 0; i < rigidbodies.Length; i++) {
+			rigidbodies[i].isKinematic = false;
+		}
+		GetComponent<Animator>().enabled = false;
+	}
+}
