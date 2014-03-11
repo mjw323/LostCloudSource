edf4e46025acaefcf2e7e959d8a23446bc42eece
gl326 <banovg@gmail.com>
2014-03-10 20:52:48 -0400

    Yorex screen shake <3

--- Assets/Scripts/Game/Camera/Framing.cs
+++ Assets/Scripts/Game/Camera/Framing.cs
@@ -9,14 +9,17 @@ public class Framing : MonoBehaviour {
 	
 	private float screenShake = 0f;
 	private float shakeAmount = 0f;
+	[HideInInspector][SerializeField] private MotionBlur mblur;
 
 	[HideInInspector][SerializeField] new private Transform transform;
 	[HideInInspector][SerializeField] new private Camera camera;
 	[HideInInspector][SerializeField] private Vector3 translation;
+	
 
 	private void Awake() {
 		transform = GetComponent<Transform>();
 		camera = GetComponent<Camera>();
+		mblur = GetComponent<MotionBlur>();
 	}
 
 	private void LateUpdate() {
@@ -36,16 +39,21 @@ public class Framing : MonoBehaviour {
 		
 		/////////////////screen shake///////////////
 		if (screenShake > 0f){
-			translation.x += Mathf.Sin(4f * Mathf.PI * screenShake) * shakeAmount * 0.5f * Mathf.Min (1f,screenShake / 0.5f);
-			translation.y += Mathf.Sin(6f * Mathf.PI * screenShake) * shakeAmount * Mathf.Min (1f,screenShake / 0.5f);
+			translation.x += Mathf.Sin(8f * Mathf.PI * screenShake) * shakeAmount * 0.5f * Mathf.Min (1f,screenShake / 0.5f);
+			translation.y += Mathf.Sin(12f * Mathf.PI * screenShake) * shakeAmount * Mathf.Min (1f,screenShake / 0.5f);
 			screenShake -= Time.deltaTime;
+			
+			mblur.enabled = true;
+			mblur.blurAmount = shakeAmount * Mathf.Min (1f,screenShake / 0.5f);
 		}
+		else{mblur.enabled = false;}
 		
 		/////////////////set///////////////
 		transform.Translate(translation);
 	}
 	
 	public void ShakeScreen(float secs, float amt){
+		Debug.Log ("shaking screen by "+amt+" for "+secs+" seconds!");
 		screenShake = secs;
 		shakeAmount = amt;
 	}
