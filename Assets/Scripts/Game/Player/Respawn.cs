7b3d5fa4a18039598c6e4b6d383237198d0a5c20
Greer <zag27@n1-142-137.dhcp.drexel.edu>
2014-04-12 17:00:46 -0400

    Began revising death/respawn system for ragdolling and probably node-based respawn locations.

---
+++ Assets/Scripts/Game/Player/Respawn.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private float fadeInSeconds;
+    [SerializeField] private float fadeOutSeconds;
+    [SerializeField] private float locationUpdateIntervalSeconds;
+    [SerializeField] private Transform player;
+    private Vector3 location;
+    private bool inProgress;
+
+    private const float checkDistance = 10f;
+
+    public void BeginRespawn()
+    {
+        if (!inProgress) {
+            StartCoroutine(DoRespawn());
+        }
+    }
+
+    private IEnumerator DoRespawn()
+    {
+        yield return new WaitForSeconds(fadeOutSeconds);
+        player.position = location;
+        yield return new WaitForSeconds(fadeInSeconds);
+    }
+
+    private void UpdateLocation()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(player.position, -Vector3.up, out hit, checkDistance)) {
+            if (hit.transform.tag == "Terrain") {
+                respawnLocation = transform.position;
+            }
+        }
+    }
+
+    private IEnumerator DoUpdateLocation()
+    {
+        while (true) {
+            yield return new WaitForSeconds(locationUpdateIntervalSeconds);
+            UpdateLocation();
+            yield return null;
+        }
+    }
+
+    private void Awake()
+    {
+        inProgress = false;
+        StartCoroutine(DoUpdateLocation());
+    }
+}
