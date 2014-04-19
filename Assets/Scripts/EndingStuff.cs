7ea07c8c6d01529b4656f7d011f45a01445ef18e
gl326 <banovg@gmail.com>
2014-04-18 23:45:34 -0400

    Ending stuff gets switched on/off

---
+++ Assets/Scripts/EndingStuff.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingStuff : MonoBehaviour {
+	private UpgradeSystem noke;
+	private bool ending = false;
+
+	// Use this for initialization
+	void Start () {
+		noke = GameObject.FindWithTag ("Player").GetComponent<UpgradeSystem> ();
+		
+
+		for (int i=0; i<this.transform.childCount; i+=1){ //put the kids to sleep
+			this.transform.GetChild(i).gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!ending && noke.UpgradesFound()>=3){
+			ending = true;
+			for (int i=0; i<this.transform.childCount; i+=1){ //wake up, kids!!! the game's gonna end!!!
+				this.transform.GetChild(i).gameObject.SetActive(true);
+			}
+		}
+	}
+}
