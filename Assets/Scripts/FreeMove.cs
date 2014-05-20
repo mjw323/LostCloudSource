c23af0881900ddfca233b3964c927a7c5678daa7
Michael Wolfe <mjw323@drexel.edu>
2014-05-19 22:44:25 -0400

    Created a free moving object controlled for flythroughs

---
+++ Assets/Scripts/FreeMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FreeMove : MonoBehaviour {
+	
+	void Start()
+	{
+
+	}
+	
+	void Update()
+	{
+		float stickX = Input.GetAxis("Horizontal");
+		float stickY = Input.GetAxis("Vertical");
+		Vector3 stickDirection = new Vector3(stickX, 0f, stickY);
+
+		this.transform.Translate(stickDirection* 2, Space.World);
+
+		if(Input.GetButton ("Jump")){
+			this.transform.Translate (Vector3.up, Space.World);
+		}
+
+		if(Input.GetButton ("Grind")){
+			this.transform.Translate (Vector3.down, Space.World);
+		}
+	
+	}
+
+}
